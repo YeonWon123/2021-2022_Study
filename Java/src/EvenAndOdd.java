b2dd4746c145c22a6fec73d68c2c28e276e64e1e
남윤원 <weonwon123@naver.com>
2021-11-28 16:43:48 +0900

    Create a EvenAndOdd.java (#1)
    프로그래머스 짝수와 홀수 문제풀이

---
+++ Java/src/EvenAndOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenAndOdd {
+
+    public static String solution_yeonwon(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+
+    public static void main(String[] args) {
+        System.out.print("정수 num 입력: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        System.out.println(EvenAndOdd.solution_yeonwon(num));
+    }
+
+}
