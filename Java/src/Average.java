fa08272ee87f45d3248dbd4e9ceed64404d990c4
NaSeonghyun <shgdx12@naver.com>
2021-11-28 17:22:19 +0900

    Average update commit(seonghyun)

--- Java/src/Average.java
+++ Java/src/Average.java
@@ -12,6 +12,15 @@ public class Average {
         return answer / arr.length;
     }
 
+    public static double solution_seonghyun(int[] arr) {
+        double answer = 0;
+        for(int i=0; i<arr.length; i++){
+            answer += arr[i];
+        }
+        answer /= arr.length;
+        return answer;
+    }
+
     public static void main(String[] args) {
         int[] arr1 = {1,2,3,4};
         int[] arr2 = {5,5};
@@ -19,6 +28,8 @@ public class Average {
         System.out.println(Average.solution_yeonwon(arr1));
         System.out.println(Average.solution_yeonwon(arr2));
 
+        System.out.println(Average.solution_seonghyun(arr1));
+        System.out.println(Average.solution_seonghyun(arr2));
     }
 
 }
