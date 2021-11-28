93fceae752ab7b5f69a14fc454c36d226396a7e2
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2021-11-28 17:08:21 +0900

    EvenAndOdd.java update commit(solution_seonghyun) (#2)
    * EvenAndOdd.java update commit(solution_seonghyun)
    * EvenAndOdd update : Enter

--- Java/src/EvenAndOdd.java
+++ Java/src/EvenAndOdd.java
@@ -5,6 +5,10 @@ public class EvenAndOdd {
     public static String solution_yeonwon(int num) {
         return num % 2 == 0 ? "Even" : "Odd";
     }
+    
+    public static String solution_seonghyun(int num) {
+        return (num % 2 == 0) ? "Even" : "Odd" ;
+    }
 
     public static void main(String[] args) {
         System.out.print("정수 num 입력: ");
@@ -12,6 +16,7 @@ public class EvenAndOdd {
         int num = scanner.nextInt();
 
         System.out.println(EvenAndOdd.solution_yeonwon(num));
+        System.out.println(EvenAndOdd.solution_seonghyun(num));
     }
 
 }
